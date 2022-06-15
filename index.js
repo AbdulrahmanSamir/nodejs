befbe5143e2f79156e750ccac6c5a48ec088af59
Samir <ab.samir.asa@gmail.com>
2022-06-15 11:13:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express()
 
 const port = process.env.PORT || 3030;
 
-app.get('/', (req, res) => res.send('Hello World!! V01'))
+app.get('/', (req, res) => res.send('nodejs Hello World!! V01'))
 
 app.listen(port, (err) => {
     if (err) {
       console.log('Error::', err);
     }
-      console.log(`Onexlab app listening on port ${port}`);
+      console.log(`nodejs app listening on port ${port}`);
   });
