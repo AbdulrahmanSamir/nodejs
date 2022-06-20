9574f3c1bb0ef64ef737225935e7974cb1c82754
Samir <ab.samir.asa@gmail.com>
2022-06-20 10:34:20 +0200

    v01

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 const port = process.env.PORT || 3030;
 
-app.get('/', (req, res) => res.send('nodejs Hello World!! V01'))
+app.get('/', (req, res) => res.send('nodejs demo, Hello World!! V01'))
 
 app.listen(port, (err) => {
     if (err) {
