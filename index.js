bb1f6e00ce34897d2384b0cb35099c2566c38e86
Samir <ab.samir.asa@gmail.com>
2023-06-13 15:28:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 const port = process.env.PORT || 3030;
 
-app.get('/', (req, res) => res.send(' ECS demo, Hello NTI Team!! '))
+app.get('/', (req, res) => res.send(' ECS demo, Hello NTI MWS'))
 
 app.listen(port, (err) => {
     if (err) {
